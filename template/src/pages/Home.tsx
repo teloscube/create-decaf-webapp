332e4bc72a5146c11d0a37fc269e196f50a3420e
Ali Oguzhan Yildiz <aoguzhanyildiz@gmail.com>
2023-05-11 17:51:00 +0300

    chore: update template homepage

--- template/src/pages/Home.tsx
+++ template/src/pages/Home.tsx
@@ -1,16 +1,14 @@
-import { Col, Row, Space, Typography } from "antd";
+import { Col, Row, Typography } from 'antd';
 
 export default function Home() {
   return (
-    <Space direction="vertical" size={"middle"} style={{ width: "100%" }}>
-      <Row gutter={12} align="middle" justify={"center"}>
-        <Col span={6} style={{ textAlign: "center" }}>
-          <Typography.Title level={3}>Learn DECAF</Typography.Title>
-          <Typography.Link style={{ fontSize: '1.3em' }} href="https://docs.decafhub.com" target={'_blank'}>
-            Documentation
-          </Typography.Link>
-        </Col>
-      </Row>
-    </Space>
+    <Row gutter={12} align="middle" justify={'center'}>
+      <Col span={6} style={{ textAlign: 'center' }}>
+        <Typography.Title level={3}>Learn DECAF</Typography.Title>
+        <Typography.Link style={{ fontSize: '1.3em' }} href="https://docs.decafhub.com" target={'_blank'}>
+          Documentation
+        </Typography.Link>
+      </Col>
+    </Row>
   );
 }
