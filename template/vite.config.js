c08516b57d8b44ddb44887e318d29f145a3025d1
Ali Oguzhan Yildiz <aoguzhanyildiz@gmail.com>
2023-05-30 23:55:19 +0300

    fix: generate version.json after build

--- template/vite.config.js
+++ template/vite.config.js
@@ -1,6 +1,8 @@
 import react from '@vitejs/plugin-react';
+import { writeFileSync } from 'fs';
 import { defineConfig, loadEnv } from 'vite';
 import checker from 'vite-plugin-checker';
+import { version } from './package.json';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
@@ -20,6 +22,12 @@ export default defineConfig(({ mode }) => {
           lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
         },
       }),
+      {
+        name: 'generate-version-json',
+        writeBundle: () => {
+          writeFileSync('./build/version.json', JSON.stringify({ version }));
+        },
+      },
     ],
     server: {
       port: 3000,
