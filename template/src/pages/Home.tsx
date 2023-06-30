31ec381315d2726c8c921705e2fb532348b38404
Ali Oguzhan Yildiz <aoguzhanyildiz@gmail.com>
2023-06-30 21:21:07 +0300

    chore: wrap example home page with the new <Page/> layout

--- template/src/pages/Home.tsx
+++ template/src/pages/Home.tsx
@@ -1,14 +1,17 @@
+import { Page } from '@decafhub/decaf-react-webapp';
 import { Col, Row, Typography } from 'antd';
 
 export default function Home() {
   return (
-    <Row gutter={12} align="middle" justify={'center'}>
-      <Col span={6} style={{ textAlign: 'center' }}>
-        <Typography.Title level={3}>Learn DECAF</Typography.Title>
-        <Typography.Link style={{ fontSize: '1.3em' }} href="https://docs.decafhub.com" target={'_blank'}>
-          Documentation
-        </Typography.Link>
-      </Col>
-    </Row>
+    <Page>
+      <Row gutter={12} align="middle" justify={'center'}>
+        <Col span={6} style={{ textAlign: 'center' }}>
+          <Typography.Title level={3}>Learn DECAF</Typography.Title>
+          <Typography.Link style={{ fontSize: '1.3em' }} href="https://docs.decafhub.com" target={'_blank'}>
+            Documentation
+          </Typography.Link>
+        </Col>
+      </Row>
+    </Page>
   );
 }
