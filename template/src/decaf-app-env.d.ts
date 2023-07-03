e9a1cb93910286fefd1a3e1614dc2d60a4e5ff5e
Ali Oguzhan Yildiz <aoguzhanyildiz@gmail.com>
2023-07-04 00:17:19 +0300

    chore: reformat files

--- template/src/decaf-app-env.d.ts
+++ template/src/decaf-app-env.d.ts
@@ -4,10 +4,10 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-    readonly PUBLIC_URL: string;
-    readonly DECAF_WEBAPP_HOST: string;
-  }
-  
-  interface ImportMeta {
-    readonly env: ImportMetaEnv;
-  }
+  readonly PUBLIC_URL: string;
+  readonly DECAF_WEBAPP_HOST: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
