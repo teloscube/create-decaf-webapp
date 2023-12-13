3d6093520b3b514d07c9d6e7792e249560b5e82e
Ali Oguzhan Yildiz <aoguzhanyildiz@gmail.com>
2023-12-13 10:20:51 +0300

    chore: upgrade nvmrc and actions to use Node 20

--- template/shell.nix
+++ template/shell.nix
@@ -1,9 +1,9 @@
-with import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/23.05.tar.gz") { };
+with import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/23.11.tar.gz") { };
 
 stdenv.mkDerivation {
   name = "--projectname--";
 
   buildInputs = with pkgs; [
-    nodejs_18
+    nodejs_20
   ];
 }
